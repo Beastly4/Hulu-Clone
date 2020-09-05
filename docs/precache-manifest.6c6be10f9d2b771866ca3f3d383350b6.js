6a3441a588fdc5cd42e472b20777a9244c395be3
Pasha <49074447+Beastly4@users.noreply.github.com>
2020-09-05 14:48:16 +0300

    Add files via upload
    build version

---
+++ docs/precache-manifest.6c6be10f9d2b771866ca3f3d383350b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be72ce2770a7ddbd779da35384dbfda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef9b7c23e01a2397d77d",
+    "url": "/static/css/main.dcdec615.chunk.css"
+  },
+  {
+    "revision": "2c6a153e5f5f69fa7104",
+    "url": "/static/js/2.ec2fc60e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ec2fc60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef9b7c23e01a2397d77d",
+    "url": "/static/js/main.082aa329.chunk.js"
+  },
+  {
+    "revision": "e23f96d817477a4cb65a",
+    "url": "/static/js/runtime-main.268a3519.js"
+  }
+]);
